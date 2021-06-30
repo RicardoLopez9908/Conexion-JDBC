3b67f24081657b6d9c70acaa15e222244bd2b804
Ricardo <57273461+RicardoLopez9908@users.noreply.github.com>
2021-06-29 21:24:51 -0300

    Update EjemploJDBC4.java

--- JDBC/src/bdd/EjemploJDBC4.java
+++ JDBC/src/bdd/EjemploJDBC4.java
@@ -4,6 +4,8 @@ import java.sql.*;
 import java.util.*;
 import java.io.*;
 
+//EJEMPLO DE CONEXION JDBC A BASE DE DATOS MYSQL UTILIZANDO UN ARCHIVO DE TEXTO PARA OBTENER LAS ESPECIFICACIONES NECESARIAS. (RECOMENDABLE)
+
 public class EjemploJDBC4 {
 	public static void main(String args[]) {
 		try {
@@ -22,15 +24,15 @@ public class EjemploJDBC4 {
 			String usuario = p.getProperty("Usuariojdbc");
 			// Inicializar el valor de url al de la propiedad Clavejdbc
 			String clave = p.getProperty("Clavejdbc");
-			// Conexión a la base de datos representada por la url
+			// ConexiÃ³n a la base de datos representada por la url
 			// con el nombre de usuario "usuario" y la clave "clave"
 			Connection con = DriverManager.getConnection(url, usuario, clave);
 			System.out.println("CONECTADO A    ---> " + url);
 
-			// Utilizar la conexión para crear el objeto sentencia
+			// Utilizar la conexiÃ³n para crear el objeto sentencia
 			Statement stmt = con.createStatement();
 
-			// Ejecución de la consulta usando el objeto de tipo
+			// EjecuciÃ³n de la consulta usando el objeto de tipo
 			// Statement para obtener el ResultSet
 			String consulta = "SELECT * FROM Cliente ORDER BY NomCliente";
 			ResultSet rs = stmt.executeQuery(consulta);
